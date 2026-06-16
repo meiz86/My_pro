334b349f30af6bb1fcbae104a3d29d4558f7cc45
meiz86 <devops_258@yahoo.com>
2026-06-16 22:25:04 +0330

    Next

--- bin/app.js
+++ bin/app.js
@@ -20,7 +20,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "..", "public")));
+app.use(express.static(path.join(__dirname, "..", "public", "css")));
 app.use("/", indexRouter);
 // a session secret is simply used to compute hash
 /*
